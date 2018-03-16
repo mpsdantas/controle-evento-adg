2269d055fc78bfd84e19a69d35e4a57a2003cf16
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2018-03-16 13:03:24 -0300

    inserção usuarios csv

--- src/controllers/dashboard/dashboard.js
+++ src/controllers/dashboard/dashboard.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Usuarios = mongoose.model('Usuarios')
+const Participantes = mongoose.model('Participantes')
 const crypto = require('crypto')
 const path = require('path')
 const fileUpload = require('express-fileupload')
@@ -11,32 +12,37 @@ exports.renderDashboard = (application, req, res) => {
 exports.renderPopularBanco = (application,req, res) => {
     res.render('popular-banco',{erro:false,sucesso:false})
 }
-exports.uploadPopularBanco = (application, req, res) => {
+exports.uploadPopularBanco = async (application, req, res) => {
     const sampleFile = req.files.arquivo
     if(sampleFile==undefined || path.extname(sampleFile.name)!=='.csv'){
         res.render('popular-banco',{erro:true,sucesso:false})
     }
-    sampleFile.mv("src/temp/arquivoTemp.csv", (err) => {
+    sampleFile.mv("src/temp/arquivoTemp.csv", async (err) => {
         if (err) return handleError(err);
         var urlSalvar = __dirname
         urlSalvar = urlSalvar.replace("controllers/dashboard", "temp");
         console.log(urlSalvar)
         var data = fs.readFileSync(path.join(urlSalvar, 'arquivoTemp.csv'), { encoding : 'utf8'});
         var options = {
-            delimiter : ',', // optional
-            quote     : '"' // optional
-          };
-          // for multiple delimiter you can use regex pattern like this /[,|;]+/
-           
-          /* 
-            for importing headers from different source you can use headers property in options 
-            var options = {
-              headers : "sr,name,age,gender"
-            };
-          */
-           
-        console.log(csvjson.toObject(data, options));
-    });
-    
-    
+            delimiter : ',',
+            quote     : '"' 
+        };
+        const arrayParticipantes = csvjson.toObject(data, options)
+        //console.log(csvjson.toObject(data, options));
+        for(let i =0;i<arrayParticipantes.length;i++){
+            for(let j=0;j<3;j++){
+                arrayParticipantes[i].cpf = arrayParticipantes[i].cpf.replace(".","",arrayParticipantes[i].cpf)
+                arrayParticipantes[i].cpf = arrayParticipantes[i].cpf.replace("-","",arrayParticipantes[i].cpf)
+            }
+            arrayParticipantes[i].checkin = 0;
+            arrayParticipantes[i].checkout = 0;
+            let novoParticipante = new Participantes(arrayParticipantes[i])
+            await novoParticipante.save();
+        }
+        fs.unlink(urlSalvar+"/arquivoTemp.csv", function(err){
+            if(err) console.log(err)
+            console.log("Arquivo salvo")
+        }); 
+        res.render('popular-banco',{erro:false,sucesso:true})
+    });  
 }
---
+++ src/schemas/participantes.js
@@ -0,0 +1,15 @@
+const mongoose  = require('mongoose');
+const Schema = mongoose.Schema;
+
+module.export = mongoose.model('Participantes', new Schema({
+  nome:String,
+  cpf:String,
+  datanascimento:String,
+  email:String,
+  telefone: String,
+  sexo:String,
+  statuscheckin: Boolean,
+  checkin: Number,
+  checkout: Number
+
+}))
