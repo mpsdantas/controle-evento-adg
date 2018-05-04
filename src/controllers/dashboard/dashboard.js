310ba9770df1d10e1d2e9c793d34b1e992b75698
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-04 09:36:34 -0300

    Ajustes no controlador do dashboard

--- src/controllers/dashboard/dashboard.js
+++ src/controllers/dashboard/dashboard.js
@@ -6,30 +6,31 @@ const path = require('path');
 const fileUpload = require('express-fileupload');
 const csvjson = require('csvjson');
 const fs = require('fs');
+
 exports.renderDashboard = async (application, req, res) => {
     const totalParticipantes = await Participantes.find({});
     var totalPresentes = 0;
-    for(let i=0; i<totalParticipantes.length; i++) 
-        if(totalParticipantes[i].statuscheckin) totalPresentes++;
+    for (let i = 0; i < totalParticipantes.length; i++)
+        if (totalParticipantes[i].statuscheckin) totalPresentes++;
     const totalAusentes = totalParticipantes.length - totalPresentes;
-    res.render('dashboard',{
-        totalParticipantes:totalParticipantes.length, 
-        totalPresentes:totalPresentes,
-        totalAusentes:totalAusentes
+    res.render('dashboard', {
+        totalParticipantes: totalParticipantes.length,
+        totalPresentes: totalPresentes,
+        totalAusentes: totalAusentes
     });
 };
-exports.renderPopularBanco = (application,req, res) => {
-    res.render('popular-banco',{erro:false,sucesso:false})
+exports.renderPopularBanco = (application, req, res) => {
+    res.render('popular-banco', { erro: false, sucesso: false })
 };
 exports.uploadPopularBanco = async (application, req, res) => {
-    if(req.body.senha!=="001122334455"){
-        res.render('popular-banco',{erro:true,sucesso:false});
+    if (req.body.senha !== "001122334455") {
+        res.render('popular-banco', { erro: true, sucesso: false });
         return;
     }
     console.log(req.body.senha)
     const sampleFile = req.files.arquivo;
-    if(sampleFile==undefined || path.extname(sampleFile.name)!=='.csv'){
-        res.render('popular-banco',{erro:true,sucesso:false});
+    if (sampleFile == undefined || path.extname(sampleFile.name) !== '.csv') {
+        res.render('popular-banco', { erro: true, sucesso: false });
         return;
     }
     sampleFile.mv("src/temp/arquivoTemp.csv", async (err) => {
@@ -37,17 +38,17 @@ exports.uploadPopularBanco = async (application, req, res) => {
         var urlSalvar = __dirname;
         urlSalvar = urlSalvar.replace("controllers/dashboard", "temp");
         //console.log(urlSalvar);
-        var data = fs.readFileSync(path.join(urlSalvar, 'arquivoTemp.csv'), { encoding : 'utf8'});
+        var data = fs.readFileSync(path.join(urlSalvar, 'arquivoTemp.csv'), { encoding: 'utf8' });
         var options = {
-            delimiter : ',',
-            quote     : '"' 
+            delimiter: ',',
+            quote: '"'
         };
         const arrayParticipantes = csvjson.toObject(data, options)
         //console.log(csvjson.toObject(data, options));
-        for(let i =0;i<arrayParticipantes.length;i++){
-            for(let j=0;j<3;j++){
-                arrayParticipantes[i].cpf = arrayParticipantes[i].cpf.replace(".","",arrayParticipantes[i].cpf)
-                arrayParticipantes[i].cpf = arrayParticipantes[i].cpf.replace("-","",arrayParticipantes[i].cpf)
+        for (let i = 0; i < arrayParticipantes.length; i++) {
+            for (let j = 0; j < 3; j++) {
+                arrayParticipantes[i].cpf = arrayParticipantes[i].cpf.replace(".", "", arrayParticipantes[i].cpf)
+                arrayParticipantes[i].cpf = arrayParticipantes[i].cpf.replace("-", "", arrayParticipantes[i].cpf)
             }
             arrayParticipantes[i].checkin = 0;
             arrayParticipantes[i].checkout = 0;
@@ -55,42 +56,42 @@ exports.uploadPopularBanco = async (application, req, res) => {
             let novoParticipante = new Participantes(arrayParticipantes[i])
             await novoParticipante.save();
         }
-        fs.unlink(urlSalvar+"/arquivoTemp.csv", function(err){
-            if(err) console.log(err)
+        fs.unlink(urlSalvar + "/arquivoTemp.csv", function (err) {
+            if (err) console.log(err)
             console.log("Arquivo salvo com sucesso.")
-        }); 
-        res.render('popular-banco',{erro:false,sucesso:true})
-    });  
+        });
+        res.render('popular-banco', { erro: false, sucesso: true })
+    });
 };
-exports.buscarParticipantesEntrada = async (application, req, res) =>{
-    const buscaParticipantes = await Participantes.find({cpf:req.body.cpf,statuscheckin:false});
-    res.status(200).json({buscaParticipantes});
+exports.buscarParticipantesEntrada = async (application, req, res) => {
+    const buscaParticipantes = await Participantes.find({ cpf: req.body.cpf, statuscheckin: false });
+    res.status(200).json({ buscaParticipantes });
     return;
 };
-exports.buscarParticipantesSaida = async (application, req, res) =>{
-    const buscaParticipantes = await Participantes.find({cpf:req.body.cpf,statuscheckin:true});
-    res.status(200).json({buscaParticipantes});
+exports.buscarParticipantesSaida = async (application, req, res) => {
+    const buscaParticipantes = await Participantes.find({ cpf: req.body.cpf, statuscheckin: true });
+    res.status(200).json({ buscaParticipantes });
     return;
 };
-exports.realizarEntrada = async(application, req, res)=>{
+exports.realizarEntrada = async (application, req, res) => {
     const dataCheckIn = new Date().getTime();
-    if(req.body.idPulseira==""){
-        res.status(200).json({status:false});
+    if (req.body.idPulseira == "") {
+        res.status(200).json({ status: false });
         return
     }
-    await Participantes.update({ _id: req.body.idParticipante}, { $set: { idPulseira: req.body.idPulseira,checkin:dataCheckIn,statuscheckin:true}});
-    res.status(200).json({status:true});
+    await Participantes.update({ _id: req.body.idParticipante }, { $set: { idPulseira: req.body.idPulseira, checkin: dataCheckIn, statuscheckin: true } });
+    res.status(200).json({ status: true });
     return
 };
-exports.realizarSaida = async(application, req, res)=>{
+exports.realizarSaida = async (application, req, res) => {
     const dataCheckOut = new Date().getTime();
-    await Participantes.update({ _id: req.body.idParticipante}, { $set: {checkout:dataCheckOut}});
-    res.status(200).json({status:true});
+    await Participantes.update({ _id: req.body.idParticipante }, { $set: { checkout: dataCheckOut } });
+    res.status(200).json({ status: true });
     return
 };
-exports.setupSystem = async(application, req, res)=>{
-    if(!req.body.senha=="001122334455"){
-        res.status(200).json({status:false})
+exports.setupSystem = async (application, req, res) => {
+    if (!req.body.senha == "001122334455") {
+        res.status(200).json({ status: false })
     }
     const nomeUser = "Gim";
     const email = "gim@ect.ufrn.br";
@@ -101,5 +102,5 @@ exports.setupSystem = async(application, req, res)=>{
         "senha": senha
     })
     await novoUsuario.save();
-    res.status(200).json({status:true});
+    res.status(200).json({ status: true });
 }
