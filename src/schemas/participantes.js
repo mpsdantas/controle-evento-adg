f7e86311adfe580d008f43574653db7589176944
Marcus Dantas <mpsdantas15@gmail.com>
2018-05-04 09:36:53 -0300

    Ajuste schema participantes

--- src/schemas/participantes.js
+++ src/schemas/participantes.js
@@ -1,18 +1,27 @@
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 module.export = mongoose.model('Participantes', new Schema({
-  nome:String,
-  cpf:String,
-  datanascimento:String,
-  email:String,
-  telefone: String,
-  sexo:String,
-  statuscheckin: {
-  	type: Boolean,
-	default: false
-  },
-  checkin: Number,
-  checkout: Number,
-  idPulseira: Number
-}))
+      nome: String,
+      cpf: String,
+      datanascimento: String,
+      email: String,
+      telefone: String,
+      sexo: String,
+      statuscheckin: {
+            type: Boolean,
+            default: false
+      },
+      checkin: {
+            type: Number,
+            default: 0
+      },
+      checkout: {
+            type: Number,
+            default: 0
+      },
+      idPulseira: {
+            type: Number,
+            default: 0
+      }
+}));
